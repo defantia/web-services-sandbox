02a0da967949602b7b1236256f154787bba9aacf
adefanti <aurelien.defanti@gmail.com>
2012-09-07 01:50:42 +0200

    create simple JAX-WS EJB web service

---
+++ jaxws-service-with-ejb/src/main/java/org/ws/samples/jaxws/UserService.java
@@ -0,0 +1,20 @@
+package org.ws.samples.jaxws;
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+@WebService(name = "UserWS", targetNamespace = "http://jaxws.samples.ws.org")
+public interface UserService {
+
+	@WebMethod(operationName = "getUserByName")
+	@WebResult(name = "getUserResult")
+	User getUserByName(@WebParam(name = "userName") String name);
+
+	@WebMethod(operationName = "getUsers")
+	@WebResult(name = "getUsersResult")
+	List<User> getUsers();
+}
