f8a0e01e3fe38ebaa02a533f7740850f2296b8ea
adefanti <aurelien.defanti@gmail.com>
2012-09-08 00:48:31 +0200

    Create simple CXF web service

---
+++ jaxws-service-with-cxf/src/main/java/org/ws/samples/jaxws/cxf/User.java
@@ -0,0 +1,23 @@
+package org.ws.samples.jaxws.cxf;
+
+public class User {
+
+	private String name;
+	private String surname;
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
