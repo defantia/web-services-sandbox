7e6614442fdf77c720c3b7b2110e375b79bf08d2
adefanti <aurelien.defanti@gmail.com>
2012-09-10 01:46:22 +0200

    create new sample project containing web service with metro stack

---
+++ jaxws-service-with-metro/src/main/java/org/ws/samples/jaxws/pojo/UserService.java
@@ -0,0 +1,21 @@
+package org.ws.samples.jaxws.pojo;
+
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+@WebService
+public interface UserService {
+
+	@WebMethod(operationName = "getUserByName")
+	@WebResult(name = "getUserResult")
+	User getUserByName(@WebParam(name = "userName") String name);
+
+	@WebMethod(operationName = "getUsers")
+	@WebResult(name = "getUsersResult")
+	List<User> getUsers();
+}
