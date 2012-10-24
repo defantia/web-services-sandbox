e371d482cf3674e1cf7e26be86cd04102c9aea83
adefanti <aurelien.defanti@gmail.com>
2012-10-25 01:21:44 +0200

    changed parameter name with webparam annotation

--- jaxws-service-with-cxf/src/main/java/org/ws/samples/jaxws/cxf/HelloWebService.java
+++ jaxws-service-with-cxf/src/main/java/org/ws/samples/jaxws/cxf/HelloWebService.java
@@ -1,11 +1,12 @@
 package org.ws.samples.jaxws.cxf;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
 public interface HelloWebService {
 
 	@WebMethod
-	String hello(final String name);
+	String hello(@WebParam(name = "name") final String name);
 }
