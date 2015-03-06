e16e2ae52f7c1cfd9f6c48000d0ab3a3582b986c
adefanti <aurelien.defanti@gmail.com>
2015-03-07 00:00:29 +0100

    Annoted entity defined in persistence.xml

--- jaxws-service-with-ejb/src/main/java/org/ws/samples/jaxws/ejb/model/User.java
+++ jaxws-service-with-ejb/src/main/java/org/ws/samples/jaxws/ejb/model/User.java
@@ -1,9 +1,26 @@
 package org.ws.samples.jaxws.ejb.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+	@Id
+	@GeneratedValue
+	private long id;
 
 	private String name;
 	private String surname;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 	public String getSurname() {
 		return surname;
